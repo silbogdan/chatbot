96bd0020104306d1e653ee02dfa5396d7b88229f
MunteanuRaresEugen <74292996+MunteanuRaresEugen@users.noreply.github.com>
2021-11-17 20:05:51 +0200

    Added content on About Us

--- ChatBot/AboutUsPanel.cpp
+++ ChatBot/AboutUsPanel.cpp
@@ -3,5 +3,5 @@
 AboutUsPanel::AboutUsPanel(wxPanel* parent)
 	: wxPanel(parent, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxBORDER_SUNKEN)
 {
-	m_text = new wxStaticText(this, -1, wxT("This is the AboutUs panel!"), wxPoint(0, 0));
+	m_text = new wxStaticText(this, -1, wxT("The contributors of this Chatbot are:\n Silvasan Bogdan\n Macovei Radu-Marian\n Ionita Ionut \n Cretan Adrian \n Munteanu Rares"), wxPoint(0, 0));
 }
