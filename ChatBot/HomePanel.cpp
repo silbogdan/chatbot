a03f562fd1ab523a399817f2a8ebb6c548fd20ad
Bogdan Silvasan <bogdan.silve@gmail.com>
2021-12-27 15:38:32 +0200

    Removed demonstration on home page

--- ChatBot/HomePanel.cpp
+++ ChatBot/HomePanel.cpp
@@ -4,8 +4,5 @@
 HomePanel::HomePanel(wxPanel* Parent)
 	: wxPanel(Parent, -1, wxDefaultPosition, wxDefaultSize, wxBORDER_SUNKEN)
 {
-	PDFReader* pdf = new PDFReader("BioprocessInfo\\2.txt");
-	std::string textToPrint = pdf->printText();
-
-	m_text = new wxStaticText(this, -1, wxString::FromUTF8(textToPrint.substr(textToPrint.size() - 30)), wxPoint(0, 0));
+	m_text = new wxStaticText(this, -1, wxString::FromUTF8("Hello from Home Page"), wxPoint(0, 0));
 }
