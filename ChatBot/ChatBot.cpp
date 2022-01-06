6cb5af96dab9ab94536849fca3d65eda6f9459f9
radu <radu_marian.macovei@stud.acs.upb.ro>
2022-01-06 14:21:14 +0200

    Fixed bugs regarding message reception

--- ChatBot/ChatBot.cpp
+++ ChatBot/ChatBot.cpp
@@ -15,7 +15,7 @@ EVT_MENU(wxID_ABOUT, MyFrame::OnAbout)
 EVT_BUTTON(BUTTON_CHATBOT, MyFrame::ShowChatbot)
 EVT_BUTTON(BUTTON_ABOUT_US, MyFrame::ShowAboutUs)
 EVT_BUTTON(BUTTON_SEND, ChatbotPanel::takeMessage)
-//EVT_LIST_
+EVT_TEXT_ENTER(wxID_HIGHEST + 10, ChatbotPanel::takeMessage)
 wxEND_EVENT_TABLE()
 wxIMPLEMENT_APP(MyApp);
 
--- ChatBot/ChatbotPanel.cpp
+++ ChatBot/ChatbotPanel.cpp
@@ -8,7 +8,7 @@ ChatbotPanel::ChatbotPanel(wxPanel* parent)
 	: wxPanel(parent, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxBORDER_SUNKEN)
 {	
 	wxBitmap* bitmap = new wxBitmap;
-	text_box = new wxTextCtrl(this, wxID_ANY, _("Type a message..."), wxPoint(0, 580), wxSize(300, 20), 0, wxDefaultValidator, _T("ID_TEXTCTRL1"));
+	text_box = new wxTextCtrl(this, wxID_HIGHEST + 10, _("Type a message..."), wxPoint(0, 580), wxSize(300, 20), wxTE_PROCESS_ENTER, wxDefaultValidator, _T("ID_TEXTCTRL1"));
 	bitmap->LoadFile("send.png", wxBITMAP_TYPE_PNG);
 	send_button = new wxBitmapButton(this, BUTTON_SEND, *bitmap, wxPoint(300, 580), wxSize(10, 10), wxBORDER_NONE);
 	main_chat = new wxListCtrl(this, wxID_ANY, wxPoint(100, 100), wxSize(200, 200), wxLC_REPORT | wxLC_VRULES);
@@ -36,6 +36,8 @@ void ChatbotPanel::takeMessage(wxCommandEvent& event)
 	temp = new Message();
 
 	temp->msg = text_box->GetValue();
+	if (temp->msg == "")
+		return;
 	temp->isbot = 0;
 	
 	main_chat->InsertItem(i, _(""));
